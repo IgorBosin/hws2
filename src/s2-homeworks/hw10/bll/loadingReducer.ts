30cbab80df3e43240acb19d5f115585437edcab9
IgorBosin <ibosin@yandex.ru>
2023-09-26 21:27:02 +0700

    hw10 (completed)

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,10 +2,14 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+type initStateType = typeof initState
+
+export const loadingReducer = (state:initStateType = initState, action: LoadingActionType): initStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case "CHANGE_LOADING":{
+            return {...state, isLoading: action.isLoading}
+        }
         default:
             return state
     }
